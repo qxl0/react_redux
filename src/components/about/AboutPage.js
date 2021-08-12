8f1a869cd5524423aa5beec23187209ae455ed03
qxl0 <qxl0@yahoo.com>
2021-08-12 11:20:36 -0400

    initial commit: add course

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const AboutPage = () => (
+    <div> 
+        <h2>About</h2>
+        <p>
+            This is the about page.
+        </p>
+    </div>
+);
+
+export default AboutPage;
