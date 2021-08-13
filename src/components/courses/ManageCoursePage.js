f044dce5ad16cf9908d1e28685e8d76d2b3bc63c
qxl0 <qxl0@yahoo.com>
2021-08-13 13:14:28 -0400

    added managecourse/addcourse form

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,53 +1,58 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { loadCourses } from '../../redux/actions/courseActions';
-import { loadAuthors } from '../../redux/actions/authorActions';
-import PropTypes  from 'prop-types';
-
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
-
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { loadCourses } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+	const [errors, setErrors] = useState({});
+  useEffect(() => {
     if (courses.length === 0) {
-      loadCourses().catch(error => {
+      loadCourses().catch((error) => {
         console.log(error);
       });
     }
 
     if (authors.length === 0) {
-      loadAuthors().catch(error => {
+      loadAuthors().catch((error) => {
         console.log(error);
       });
     }
-  }
- 
-  render() {
-    return (
-      <>
-        <h2>Manage Courses</h2>
-      </>
-    )
-  }
+  }, []);
+
+  return (
+   <CourseForm course={course} errors={errors} authors={authors} /> 
+  );
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
-  loadAuthors: PropTypes.func.isRequired
+  loadAuthors: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
-    // debugger;
-    return {
-        courses: state.courses,
-        authors: state.authors
-    };
+  // debugger;
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors,
+  };
 }
 
 const mapDispatchToProps = {
-    loadCourses,
-    loadAuthors,
+  loadCourses,
+  loadAuthors,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
