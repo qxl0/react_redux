8db8901364c55dde66310c5afec06ef72d68ee81
qxl0 <qxl0@yahoo.com>
2021-08-13 16:37:22 -0400

    added clientside validation

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,7 +5,7 @@ import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
-import  Spinner  from "../common/Spinner";
+import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
 function ManageCoursePage({
@@ -44,16 +44,31 @@ function ManageCoursePage({
     }));
   }
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+
+    setErrors(errors);
+    // Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
-    saveCourse(course).then(() => {
-      toast.success("Course saved!");
-      history.push("/courses");
-    }).catch(error => {
-      setSaving(false);
-      setErrors({ onSave: error.message });
-    });
+    saveCourse(course)
+      .then(() => {
+        toast.success("Course saved!");
+        history.push("/courses");
+      })
+      .catch((error) => {
+        setSaving(false);
+        setErrors({ onSave: error.message });
+      });
   }
 
   return authors.length === 0 || courses.length === 0 ? (
